34f8ac7ccb8fe2bce6614650d8a98a18b2ba4f28
Fabricio Kohlrausch <114492261+Kohlrausch@users.noreply.github.com>
2022-11-11 08:52:17 -0300

    Create At_4_teste.py

---
+++ Lista de Atividades/At_4_teste.py
@@ -0,0 +1,13 @@
+from At_4 import Agenda
+
+print ('==== Bem-Vindo a sua Agenda ====')
+
+nome=input('Digite o nome da pessoa: ')
+telefone=int(input('Digite o Telefone:'))
+endereco=input('Digite o Endereco:')
+
+print ("\n",'Dados Cadastrados na Agenda',"\n")
+
+print ('Nome:',nome)
+print ('Telefone:',telefone)
+print ('Endereco:',endereco)
