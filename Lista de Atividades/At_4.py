85f9c809dea0d134000336855d4b78ae8914b306
Fabricio Kohlrausch <114492261+Kohlrausch@users.noreply.github.com>
2022-11-11 08:51:41 -0300

    Create At_4.py

---
+++ Lista de Atividades/At_4.py
@@ -0,0 +1,9 @@
+class Agenda:
+    def __init__(self,nome):
+        self.nome = nome
+        
+    def __init__(self,telefone):
+        self.telefone = telefone
+        
+    def __init__(self,endereco):
+        self.endereco = endereco
