f1e53333efced2d03173354b4849d5e20a67296d
Fabricio Kohlrausch <114492261+Kohlrausch@users.noreply.github.com>
2022-11-11 08:52:43 -0300

    Create At_5.py

---
+++ Lista de Atividades/At_5.py
@@ -0,0 +1,14 @@
+class Cadastro:
+
+    def __init__(self,al_nome,al_endereco,al_telefone):
+        self.al_nome = al_nome
+        self.al_endereco = al_endereco
+        self.al_telefone = al_telefone
+
+    def __init__(self,pro_nome,pro_endereco,pro_telefone):
+        self.pro_nome = pro_nome
+        self.pro_endereco = pro_endereco
+        self.pro_telefone = pro_telefone
+
+    def __init__(self,cursos):
+        self.cursos = cursos
