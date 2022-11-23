f5aa30e9b1850721748937de4ec1e5eaec416e44
Fabricio Kohlrausch <114492261+Kohlrausch@users.noreply.github.com>
2022-11-22 22:53:09 -0300

    Create carro.py

---
+++ Atividade Remota de Programação II/carro.py
@@ -0,0 +1,7 @@
+class Carro:
+
+    def __init__(self,consumo,km,capacidade_tanque) :
+        
+        self.consumo = consumo
+        self.km = km
+        self.capaciade_tanque = capacidade_tanque 
